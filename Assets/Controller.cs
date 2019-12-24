851119ee3154339181b52b90df3c52994becff35
Knose1 <34002600+Knose1@users.noreply.github.com>
2019-12-24 01:01:57 +0100

    Added example

---
+++ Assets/Controller.cs
@@ -0,0 +1,47 @@
+using Com.Github.Knose1.InputUtils.Utils;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace Com.Github.Knose1.InputUtils.InputController {
+
+	//This is an example of a fully parameterized Controller.cs
+	public partial class Controller
+	{
+		private UnityInputSystem input;
+		public UnityInputSystem Input { get => input; }
+		
+		partial void InitControllerProject()
+		{
+			input = new UnityInputSystem();
+		}
+		
+		partial void GetRebindingFunctionsEditor()
+		{
+			rebindingFunctionsEditor = new List<RebindingFunction>();
+			rebindingFunctionsEditor.Add(new RebindingFunction(RebindLeft, nameof(RebindLeft), ""));
+			rebindingFunctionsEditor.Add(new RebindingFunction(RebindRight, nameof(RebindRight), ""));
+		}
+		partial void GetRebindingFunctions()
+		{
+			rebindingFunctions = new List<RebindingFunction>();
+			rebindingFunctions.Add(new RebindingFunction(RebindLeft, nameof(RebindLeft), InputSystemUtils.GetName(input.Gameplay.Left)));
+			rebindingFunctions.Add(new RebindingFunction(RebindRight, nameof(RebindRight), InputSystemUtils.GetName(input.Gameplay.Right)));
+		}
+		
+		public void RebindLeft()
+		{
+			Rebind(input.Gameplay.Left, new List<InputAction>() { input.Gameplay.Right });
+		}
+		
+		public void RebindRight()
+		{
+			Rebind(input.Gameplay.Right, new List<InputAction>() { input.Gameplay.Left });
+		}
+		public void OnDestroy()
+		{
+			DisposeInstance();
+			
+			input.Disable(); 
+		}
+	}
+}
---
+++ Assets/UnityInputSystem.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/UnityInputSystem.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @UnityInputSystem : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public @UnityInputSystem()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""UnityInputSystem"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""a1b07abb-a401-4740-a6a2-0ec09a1f3730"",
+            ""actions"": [
+                {
+                    ""name"": ""Left"",
+                    ""type"": ""Button"",
+                    ""id"": ""c19f0dbc-3c42-4400-a102-f1c5d3930667"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Right"",
+                    ""type"": ""Button"",
+                    ""id"": ""c345bc4c-4345-496c-bc94-a3c7a97d0ccf"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""07844030-03ee-49fb-b93b-0c30fccf9b0f"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Left"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d9a58cc6-9e17-4a03-a626-fc1b71635cf3"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Right"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Left = m_Gameplay.FindAction("Left", throwIfNotFound: true);
+        m_Gameplay_Right = m_Gameplay.FindAction("Right", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Left;
+    private readonly InputAction m_Gameplay_Right;
+    public struct GameplayActions
+    {
+        private @UnityInputSystem m_Wrapper;
+        public GameplayActions(@UnityInputSystem wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Left => m_Wrapper.m_Gameplay_Left;
+        public InputAction @Right => m_Wrapper.m_Gameplay_Right;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Left.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeft;
+                @Left.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeft;
+                @Left.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLeft;
+                @Right.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRight;
+                @Right.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRight;
+                @Right.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnRight;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Left.started += instance.OnLeft;
+                @Left.performed += instance.OnLeft;
+                @Left.canceled += instance.OnLeft;
+                @Right.started += instance.OnRight;
+                @Right.performed += instance.OnRight;
+                @Right.canceled += instance.OnRight;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    public interface IGameplayActions
+    {
+        void OnLeft(InputAction.CallbackContext context);
+        void OnRight(InputAction.CallbackContext context);
+    }
+}
